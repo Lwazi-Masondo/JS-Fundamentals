2261a537c70180ddfb074377168243865547fc4b
Nolwazi Masondo <masondop96@gmail.com>
2025-05-26 03:53:42 +0200

    bug fix

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,6 @@
 const arguments = process.argv;
 
-if (arguments[2] >= 0) {
+if (arguments[2] >= 0 || arguments[2] < 0) {
   console.log("My number: " + Math.floor(arguments[2]));
 } else {
   console.log("Not a number");
